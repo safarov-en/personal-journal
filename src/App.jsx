1dad94fe624ba58820cabe579d5c521c399e98a0
safarov-en <safarov.elkhan110@gmail.com>
2024-05-12 19:04:34 +0500

    Creating a hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
 import JournalList from './components/JournalList/JournalList';
+import { useLocalStorage } from './hooks/use-localstorage.hook';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
-const INITIAL_DATA = [
-  {
-    id: 1,
-    title: 'Подготовка к обновлению курсов',
-    post: 'Горные походы открывают удивительные природные ландшафт',
-    date: new Date(),
-  },
-  {
-    id: 2,
-    title: 'Поход в годы',
-    post: 'Думал, что очень много времени',
-    date: new Date()
+function mapItems(items) {
+  if(!items) {
+    return []
   }
-]
+  return items.map(i => ({
+    ...i,
+    date: new Date(i.date)
+  }))
+}
 function App() {
-  const [items, setItems] = useState(INITIAL_DATA)
+  const [items, setItems] = useLocalStorage('data')
   const addItem = item => {
-    setItems(oldItems => [...oldItems, {
+    setItems([...mapItems(items), {
       post: item.post,
       title: item.title,
       date: new Date(item.date),
-      id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
+      id: items ? Math.max(...items.map(i => i.id)) + 1 : 1
     }]);
   }
   return (
@@ -36,7 +31,7 @@ function App() {
       <LeftPanel>
         <Header />
         <JournalAddButton />
-        <JournalList items={items} />
+        <JournalList items={mapItems(items)} />
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addItem} />
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,7 +3,6 @@ import styles from './Input.module.css'
 import cn from 'classnames';
 
 const Input = forwardRef(function Input({ className, isValid, appearence, ...props }, ref) {
-    console.log(props, isValid)
     return (
         <input ref={ref} className={cn(className, styles['input'], {
             [styles['invalid']]: isValid,
